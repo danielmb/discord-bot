253d2813852e38bf59dd611629843a969daf898d
Daniel Mathisen Breivik <danbre14@nfk.no>
2024-05-05 20:25:51 +0200

    Deploy

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+  ],
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client?.user?.tag}`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return;
+
+  const command = message.content.split(' ')[0];
+
+  if (command === '!test') {
+    message.reply('Hello, world!');
+  }
+});
